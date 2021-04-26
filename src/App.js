d3c9a47bc4e92597dd55aef8742931263f45f0d8
AngelDOrtiz <aortizghs@gmail.com>
2021-04-26 14:08:38 -0700

    Basic app structure

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { Component } from 'react';
 import logo from './react-logo.svg';
 import './App.css';
 import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-  
-        My React App...
-        
+        <Header/>
+        <Main/>
+        <Footer/>
+
         <img src={logo} className="temp-images" alt="react logo" />
         <img src="acl-logo.png" className="temp-images" alt="acl logo" />
-      
+
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer className="Footer">
+            My wonderful list of things
+      </footer>
+    );
+  }
+}
+
+export default Footer;
